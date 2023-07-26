45ad2275db16b7c3c8ddc6bf40c86b8db2629a6d
mahamp <134096679+mahamp@users.noreply.github.com>
2023-07-26 22:06:49 +0330

    new files

---
+++ script.js
@@ -0,0 +1,24 @@
+const enterBtn = document.querySelector('.enter')
+const eventOffice = document.querySelector('.event-office')
+const toggleBtn = document.querySelector('.toggler')
+const navbarBackground = document.querySelector('.navbar-container')
+const navbarItems = document.querySelector('.navbar-items')
+
+enterBtn.addEventListener('click', () => {
+    eventOffice.classList.add('active')
+})
+enterBtn.addEventListener('click', () => {
+    setTimeout (function () {
+        toggleBtn.classList.add('btnactive');
+    } ,3000); 
+})
+enterBtn.addEventListener('click', () => {
+    setTimeout (function () {
+        navbarBackground.classList.add('nav-boarder');
+    } ,1000); 
+})
+enterBtn.addEventListener('click', () => {
+    setTimeout (function () {
+        navbarItems.classList.add('nav-items');
+    } ,2000); 
+})
